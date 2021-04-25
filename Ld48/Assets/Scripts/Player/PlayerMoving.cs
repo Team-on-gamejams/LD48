f7e293e3886da1f0aac710ea3fceeba9ae562ef7
Team-on <timonsol13@gmail.com>
2021-04-25 03:16:22 +0300

    Player jump fixes

--- Ld48/Assets/Scripts/Player/PlayerMoving.cs
+++ Ld48/Assets/Scripts/Player/PlayerMoving.cs
@@ -9,18 +9,17 @@ using NaughtyAttributes;
 using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMoving : MonoBehaviour
-{
-    [Header("Data"), Space]
-    [SerializeField] float speed = 8.0f;
+public class PlayerMoving : MonoBehaviour {
+	[Header("Data"), Space]
+	[SerializeField] float speed = 8.0f;
+	[SerializeField] float jumpForce = 6.5f;
 
-    [Header("Refs"), Space]
-    [SerializeField] Rigidbody2D rb;
-    [SerializeField] DebugText debugText;
+	[Header("Refs"), Space]
+	[SerializeField] Rigidbody2D rb;
+	[SerializeField] DebugText debugText;
+	[SerializeField] CapsuleCollider2D collider2D;
 
-    public Vector2 moveVector;
-    public float thrust = 100f;
-    public CapsuleCollider2D collider2D;
+	Vector2 moveVector;
 
 #if UNITY_EDITOR
 	private void OnValidate() {
@@ -31,50 +30,32 @@ public class PlayerMoving : MonoBehaviour
 	}
 #endif
 
-    void Start()
-    {
-        enabled = false;
-    }
+	void FixedUpdate() {
+		rb.velocity = rb.velocity.SetX(moveVector.x * speed);
 
-    void FixedUpdate()
-    {
-        Vector2 v2 = rb.velocity;
-        v2.x = moveVector.x * speed;
-        rb.velocity = v2;
+		debugText.SetText($"Speed: {rb.velocity.magnitude.ToString("0.0")} m/s");
+	}
 
-        debugText.SetText($"Speed: {rb.velocity.magnitude.ToString("0.0")} m/s");
-    }
+	public void OnMoveStart() {
 
-    public void OnMoveStart()
-    {
-        enabled = true;
-    }
+	}
 
-    public void OnMove(Vector2 vector)
-    {
-        moveVector = vector;
-    }
+	public void OnMove(Vector2 vector) {
+		moveVector = vector;
+	}
 
-    public void OnMoveStop()
-    {
-        enabled = false;
+	public void OnMoveStop() {
+		moveVector = Vector2.zero;
+	}
 
-        rb.velocity = moveVector = Vector2.zero;
+	public void Jump() {
+		if (IsGrounded())
+			rb.AddForce(transform.up * jumpForce, ForceMode2DEx.Acceleration);
+	}
 
-        debugText.SetText($"Speed: {rb.velocity.magnitude.ToString("0.0")} m/s");
-    }
-
-    public void Jump()
-    {
-        Debug.Log("Fuck");
-        if (IsGrounded())
-            rb.AddForce(transform.up * thrust, ForceMode2DEx.VelocityChange);
-    }
-
-    public bool IsGrounded()
-    {
-        RaycastHit2D raycastHit2D = Physics2D.BoxCast(collider2D.bounds.center, collider2D.bounds.size, 0f, Vector2.down * .1f);
-        Debug.Log(raycastHit2D.collider);
-        return raycastHit2D.collider != null;
-    }
+	public bool IsGrounded() {
+		RaycastHit2D raycastHit2D = Physics2D.BoxCast(collider2D.bounds.center, collider2D.bounds.size, 0f, Vector2.down * .1f);
+		Debug.Log(raycastHit2D.collider);
+		return raycastHit2D.collider != null;
+	}
 }
