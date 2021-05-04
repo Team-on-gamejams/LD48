23482739615e6d4e16594d442a5c9c1dbfadd77d
Team-on <timonsol13@gmail.com>
2021-05-04 04:02:17 +0300

    Fix popup overlaping

--- Ld48/Assets/Scripts/UI/Popup.cs
+++ Ld48/Assets/Scripts/UI/Popup.cs
@@ -9,13 +9,24 @@ using NaughtyAttributes;
 using Random = UnityEngine.Random;
 
 public class Popup : MonoBehaviour {
+	static int canvasSortOrder = 100;
 	public bool IsShowed => isShowed;
 
 	[Header("Refs"), Space]
+	[SerializeField] Canvas canvas;
 	[SerializeField] CanvasGroup cg;
 	[SerializeField] TextMeshProUGUI textField;
 
 	bool isShowed;
+
+#if UNITY_EDITOR
+	private void OnValidate() {
+		if (!canvas)
+			canvas = GetComponent<Canvas>();
+		if (!cg)
+			cg = GetComponent<CanvasGroup>();
+	}
+#endif
 
 	private void Awake() {
 		cg.alpha = 0.0f;
@@ -28,12 +39,16 @@ public class Popup : MonoBehaviour {
 	public void Show() {
 		isShowed = true;
 
+		canvas.sortingOrder = canvasSortOrder++;
+
 		LeanTween.cancel(gameObject, false);
 		LeanTweenEx.ChangeAlpha(cg, 1.0f, 0.1f).setEase(LeanTweenType.easeInOutQuad);
 	}
 
 	public void Hide() {
 		isShowed = false;
+
+		--canvasSortOrder;
 
 		LeanTween.cancel(gameObject, false);
 		LeanTweenEx.ChangeAlpha(cg, 0.0f, 0.1f).setEase(LeanTweenType.easeInOutQuad);
